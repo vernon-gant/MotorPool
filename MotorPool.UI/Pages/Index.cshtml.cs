30f86f9c7ae2199fe78e2d84e0bada97fb732839
Aleksandr Zakharov <aleks.zakharov@protonmail.com>
2024-02-24 10:59:26 +0100

    Changed welcome page

--- MotorPool.UI/Pages/Index.cshtml.cs
+++ MotorPool.UI/Pages/Index.cshtml.cs
@@ -2,15 +2,12 @@
 
 namespace MotorPool.UI.Pages;
 
-public class Index : PageModel
+public class IndexModel : PageModel
 {
 
     public void OnGet()
     {
-        if (User.Identity is { IsAuthenticated: false })
-        {
-            Response.Redirect("/Identity/Account/Login");
-        }
+
     }
 
 }
