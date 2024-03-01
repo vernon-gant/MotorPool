364c39d88b2117ca53f8f2d94df13748f3890162
Aleksandr Zakharov <aleks.zakharov@protonmail.com>
2024-03-01 11:12:20 +0100

    Utility part 1

---
+++ MotorPool.DatabaseSeeder/EnterpriseRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using MotorPool.Domain;
+using MotorPool.Persistence;
+
+namespace MotorPool.DatabaseSeeder;
+
+public interface EnterpriseRepository
+{
+
+    ValueTask<List<Enterprise>> GetEnterprisesAsync();
+
+}
+
+public class EfDatabaseRepository(AppDbContext dbContext) : EnterpriseRepository
+{
+
+    public async ValueTask<List<Enterprise>> GetEnterprisesAsync() => await dbContext.Enterprises.ToListAsync();
+
+}
---
+++ MotorPool.DatabaseSeeder/SeedingOptions.cs
@@ -0,0 +1,17 @@
+﻿using CommandLine;
+
+namespace MotorPool.DatabaseSeeder;
+
+public class SeedingOptions
+{
+
+    [Option('e', "enterprise-ids", Required = true, HelpText = "The ids of the enterprises where vehicles and drivers will be seeded.")]
+    public List<int> EnterpriseIds { get; set; } = new();
+
+    [Option('v', "vehicles-per-enterprise", Required = true, HelpText = "The number of vehicles to seed per enterprise.")]
+    public int VehiclesPerEnterprise { get; set; }
+
+    [Option('d', "drivers-per-enterprise", Required = true, HelpText = "The number of drivers to seed per enterprise.")]
+    public int DriversPerEnterprise { get; set; }
+
+}
