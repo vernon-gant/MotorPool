45d9c729423a695aeca735797ba9bb5603c851ed
Aleksandr Zakharov <aleks.zakharov@protonmail.com>
2024-03-04 12:38:11 +0100

    Page method itself and validation for paging dto

---
+++ MotorPool.Persistence/QueryObjects/GenericPaging.cs
@@ -0,0 +1,20 @@
+﻿using MotorPool.Services.Utils;
+
+namespace MotorPool.Persistence.QueryObjects;
+
+public static class GenericPaging
+{
+
+    public static IQueryable<T> Page<T>(this IQueryable<T> query, PageOptions pageOptions)
+    {
+        if (pageOptions.ElementsPerPage <= 0) throw new ArgumentOutOfRangeException(nameof(pageOptions.ElementsPerPage), "amount of elements per page must be bigger then 0");
+
+        if (pageOptions.PageNumber < 0) throw new ArgumentOutOfRangeException(nameof(pageOptions.PageNumber), "page number must be at least 0");
+
+        if (pageOptions.PageNumber != 0)
+            query = query.Skip(pageOptions.PageNumber * pageOptions.ElementsPerPage);
+
+        return query.Take(pageOptions.ElementsPerPage);
+    }
+
+}
---
+++ MotorPool.Utils/ValidationAttributes/NonNegativeAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MotorPool.Utils.ValidationAttributes;
+
+public class NonNegativeAttribute : ValidationAttribute
+{
+    public required bool CanBeNull { get; set; } = true;
+
+    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is null) return ValidationResult.Success;
+
+        if (value is not int inputValue) return new ValidationResult("The input values must be an int");
+
+        if (CanBeNull && inputValue >= 0 || !CanBeNull && inputValue >= 1) return ValidationResult.Success;
+
+        return new ValidationResult("Negative integer or can be null is not satisfied.");
+    }
+
+}
