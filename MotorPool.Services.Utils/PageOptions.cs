a57d1e27c8af64fc1017dd7b94cb9fd1877d3374
Aleksandr Zakharov <aleks.zakharov@protonmail.com>
2024-03-04 12:37:52 +0100

    Added paging DTO, paged view model and options

---
+++ MotorPool.Services.Utils/PageOptions.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using MotorPool.Utils.ValidationAttributes;
+
+namespace MotorPool.Services.Utils;
+
+public class PageOptionsDTO
+{
+
+    [NonNegative(CanBeNull = true)]
+    public int? PageNumber { get; set; }
+
+    [NonNegative(CanBeNull = false)]
+    public int? ElementsPerPage { get; set; }
+
+    public static implicit operator PageOptions(PageOptionsDTO dto)
+    {
+        return new ()
+        {
+            PageNumber = dto.PageNumber ?? PageOptions.DEFAULT_PAGE_NUMBER,
+            ElementsPerPage = dto.ElementsPerPage ?? PageOptions.DEFAULT_ELEMENTS_PER_PAGE
+        };
+    }
+
+}
+
+public class PageOptions
+{
+
+    public static readonly int DEFAULT_PAGE_NUMBER = 0;
+
+    public static readonly int DEFAULT_ELEMENTS_PER_PAGE = 100;
+
+    public required int PageNumber { get; set; }
+
+    public required int ElementsPerPage { get; set; }
+
+}
---
+++ MotorPool.Services.Utils/PagedViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MotorPool.Services.Utils;
+
+public class PagedViewModel<T> where T : class
+{
+
+    public required int PagesAfter { get; init; }
+
+    public required IEnumerable<T> Elements { get; init; }
+
+}
