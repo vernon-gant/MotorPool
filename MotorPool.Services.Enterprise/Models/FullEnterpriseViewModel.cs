deb04f547ef151362adb7c7ec2ab763ee68aa80b
Aleksandr Zakharov <aleks.zakharov@protonmail.com>
2024-03-11 10:15:33 +0100

    Added display of Local Time Zone id

--- MotorPool.Services.Enterprise/Models/FullEnterpriseViewModel.cs
+++ MotorPool.Services.Enterprise/Models/FullEnterpriseViewModel.cs
@@ -1,19 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
-
-namespace MotorPool.Services.Enterprise.Models;
-
-public class SimpleEnterpriseViewModel : EnterpriseDTO
-{
-    public int EnterpriseId { get; set; }
-
-    [Display(Name = "Founded on")]
-    public DateTime FoundedOn { get; set; }
-
-    [JsonIgnore]
-    public List<int> ManagerIds { get; set; } = new ();
-
-}
+﻿namespace MotorPool.Services.Enterprise.Models;
 
 public class FullEnterpriseViewModel : SimpleEnterpriseViewModel
 {
---
+++ MotorPool.Services.Enterprise/Models/SimpleEnterpriseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace MotorPool.Services.Enterprise.Models;
+
+public class SimpleEnterpriseViewModel : EnterpriseDTO
+{
+    public int EnterpriseId { get; set; }
+
+    [Display(Name = "Founded on")]
+    public DateOnly FoundedOn { get; set; }
+
+    [Display(Name = "Time Zone")]
+    public string TimeZoneId { get; set; } = string.Empty;
+
+    [JsonIgnore]
+    public List<int> ManagerIds { get; set; } = new ();
+
+}
