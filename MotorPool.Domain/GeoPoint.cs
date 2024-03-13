56c1118042dfe2ecba56db09fa088ff112f8ff3a
Aleksandr Zakharov <aleks.zakharov@protonmail.com>
2024-03-13 11:04:36 +0100

    Added geopoint

---
+++ MotorPool.Domain/GeoPoint.cs
@@ -0,0 +1,18 @@
+﻿namespace MotorPool.Domain;
+
+public class GeoPoint
+{
+
+    public int GeoPointId { get; set; }
+
+    public double Latitude { get; set; }
+
+    public double Longitude { get; set; }
+
+    public int VehicleId { get; set; }
+
+    public Vehicle? Vehicle { get; set; }
+
+    public DateTime RecordedAt { get; set; }
+
+}
